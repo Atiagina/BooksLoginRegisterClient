71f33e1685b941dee39beec190f96f16d935cd3d
Anna <atiagina@gmail.com>
2016-02-22 11:51:16 -0800

    Adds new methods for adding author, review and book
    Work is in progress

---
+++ App_Code/BookReviewLoginService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookReviewLoginService" in code, svc and config file together.
+public class BookReviewLoginService : IBookReviewLoginService
+{
+    BookReviewDbEntities db = new BookReviewDbEntities();
+
+    
+
+    public int ReviewerLogin(string username, string password)
+    {
+         int result = db.usp_ReviewerLogin(username, password);
+        if (result != -1)
+        {
+            var key = from k in db.Reviewers
+                      where k.ReviewerUserName.Equals(username)
+                      select new { k.ReviewerKey };
+
+            foreach (var k in key){
+
+            result = (int)k.ReviewerKey;
+        }
+        }
+        
+        return result;
+    }
+
+    public int ReviewerRegistration(ReviewerLite r)
+    {
+        Reviewer rv = new Reviewer();
+
+
+        int result = db.usp_NewReviewer(r.UserName, r.FirstName, r.LastName, r.Email, r.Password);
+        return result;
+    }
+
+
+    public int AddAuthor(string authorName)
+    {
+        int result = 1;
+        Author a = new Author();
+        a.AuthorName = authorName;
+
+        try
+        {
+
+            db.Authors.Add(a);
+            db.SaveChanges();
+        }
+        catch(Exception ex){
+        result = 0;
+        throw ex;
+        }
+
+        return result;
+      
+    }
+
+    public int AddBook(BookLite bl)
+    {
+        Author au = new Author();
+        au.AuthorName = "Don Norman";
+            bl.AuthorNames.Add(au);
+
+        Book b = new Book();
+        b.BookTitle = bl.Title;
+        b.BookISBN = bl.ISBN;
+        b.BookEntryDate = DateTime.Now;
+        if (b.Authors !=null){
+            foreach(var a in b.Authors){
+                Author author = db.Authors.FirstOrDefault(x => x.AuthorName.ToString().Equals(a));
+                 b.Authors.Add(author);
+            }
+        }
+
+        db.Books.Add(b);
+        db.SaveChanges();
+
+        return 1;
+    }
+
+    public int AddReview(ReviewLite rl)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ App_Code/IBookReviewLoginService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBookReviewLoginService" in both code and config file together.
+[ServiceContract]
+public interface IBookReviewLoginService
+{
+    [OperationContract]
+
+    int ReviewerLogin(string username, string password);
+
+    [OperationContract]
+    int ReviewerRegistration(ReviewerLite r);
+
+    [OperationContract]
+    int AddAuthor(string authorName);
+
+    [OperationContract]
+    int AddBook(BookLite bl);
+
+    [OperationContract]
+    int AddReview(ReviewLite rl);
+
+}
+
+[DataContract]
+public class ReviewerLite
+{
+
+    [DataMember]
+    public string LastName { set; get; }
+
+    [DataMember]
+    public string FirstName { set; get; }
+
+    [DataMember]
+    public string UserName { set; get; }
+
+    [DataMember]
+    public string Password { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+}
+
+[DataContract]
+public class BookLite {
+
+    [DataMember]
+    public string Title {get; set;}
+
+    [DataMember]
+    public string ISBN {get; set;}
+
+    [DataMember]
+    public List<Author> AuthorNames {get; set;}
+
+}
+
+[DataContract]
+
+public class ReviewLite
+{
+    [DataMember]
+    public string BookTitle { set; get; }
+
+    [DataMember]
+    public string ReviewerKey { set; get; }
+
+    [DataMember]
+    public string ReviewTitle { set; get; }
+
+    [DataMember]
+    public int Rating { set; get; }
+
+    [DataMember]
+    public string ReviewText { set; get; }
+
+}
